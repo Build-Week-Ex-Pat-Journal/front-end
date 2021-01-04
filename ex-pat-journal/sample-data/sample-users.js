42c9a8eac7d11b4988f51a3bda8815e0601efc6b
Michael Murphy <mmurph917@michaels-mbp.lan>
2021-01-04 10:27:39 -0600

    Monday Morning Stand Up

---
+++ ex-pat-journal/sample-data/sample-users.js
@@ -0,0 +1,40 @@
+const userList = [
+    
+    {
+    fname: 'Mike',
+    lname: 'Murphy',
+    email: 'email@email.com',
+    username: 'mike-murphy',
+    password: 'password1234',
+    posts:[
+        {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post1"},
+        {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post2"},
+        {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post3"}
+    ]
+  },
+  {
+    fname: 'Will',
+    lname: 'Fletcher',
+    email: 'email@gmail.com',
+    username: 'will-fletcher',
+    password: 'password12345',
+    posts:[
+            {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post3"},
+            {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post4"},
+            {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post5"}
+    ]
+  },
+  {
+    fname: 'Josh',
+    lname: 'Lovins',
+    email: 'hotmail@hotmail.com',
+    username: 'josh-lovins',
+    password: 'password1234',
+    posts: [
+        {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post7"},
+        {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post8"},
+        {photo:"https://images.unsplash.com/photo-1609726057321-53e1f2e8acf3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80", story:"This is a generic post9"}
+    ]
+  }
+
+]
--- ex-pat-journal/src/Posts.js
+++ ex-pat-journal/src/Posts.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Post from './Post'
 
-function Posts() {
+//////////////// INITIAL STATES ////////////////
+
+function Posts(props) {
+
+const { userList } = props
+let user = 0
+let posts = userList[]
     return(
         <div>Posts
-             <Post />
+            {
+                posts.map((post,idx) =>
+                <div key={idx} className='post card'>
+                    <Post user={user}/> 
+                </div>
+                )
+            }
+             
         </div>
        
     )
