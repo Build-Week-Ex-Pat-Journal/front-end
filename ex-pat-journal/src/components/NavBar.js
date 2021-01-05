bd273bca043177bb093c5d5760c1de295f712302
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-01-05 17:12:18 -0500

    I was able to transfer the styling and incorprate the same theme as discussed added a title to the registration and styled the forms to match the template that was done on the forked brach

--- ex-pat-journal/src/components/NavBar.js
+++ ex-pat-journal/src/components/NavBar.js
@@ -1,53 +1,57 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 // import Typography from '@material-ui/core/Typography'
 // import { ThemeProvider, withStyles } from "@material-ui/core/styles";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
 // import { border, palette } from '@material-ui/system';
 
-
 const NavBar = (props) => {
-    // console.log(props.theme);
+  // console.log(props.theme);
 
-    const logout = () => {
-        localStorage.removeItem("token");
-        // props.history.push("/");
-    }
-    return(
-        <AppBar className="nav-bar" display="flex" position="static">
-            <Toolbar>
-                <Grid justify="space-around" container spacing={24}>
-                    <Grid item style={{width: "12%"}}>
-                        <Link to="/" color="inherit">
-                            Log in
-                        </Link>
-                    </Grid>
-                    <Grid item style={{width: "12%"}}>
-                        <Link to="/all-posts" color="inherit">
-                            All Posts
-                        </Link>
-                    </Grid>
-                    <Grid item style={{width: "12%"}}>
-                        <Link to="/my-posts" color="inherit">
-                            My Posts
-                        </Link>
-                    </Grid>
-                    <Grid item style={{width: "12%"}}>
-                        <Link to="/new-post" color="inherit">
-                            New Post
-                        </Link>
-                    </Grid>
-                    <Grid item style={{width: "12%"}}>
-                        {/* to="/new-post" */}
-                        <Link to="/" onClick={logout} color="inherit">
-                            Log out
-                        </Link>
-                    </Grid>
-                </Grid>
-            </Toolbar>
-        </AppBar>
-    )
-}
+  const logout = () => {
+    localStorage.removeItem("token");
+    // props.history.push("/");
+  };
+  return (
+    <AppBar
+      className="nav-bar"
+      display="flex"
+      position="static"
+      style={{ backgroundColor: "antiquewhite" }}
+    >
+      <Toolbar>
+        <Grid justify="space-around" container spacing={24}>
+          <Grid item style={{ width: "12%" }}>
+            <Link to="/" color="inherit">
+              Log in
+            </Link>
+          </Grid>
+          <Grid item style={{ width: "12%" }}>
+            <Link to="/all-posts" color="inherit">
+              All Posts
+            </Link>
+          </Grid>
+          <Grid item style={{ width: "12%" }}>
+            <Link to="/my-posts" color="inherit">
+              My Posts
+            </Link>
+          </Grid>
+          <Grid item style={{ width: "12%" }}>
+            <Link to="/new-post" color="inherit">
+              New Post
+            </Link>
+          </Grid>
+          <Grid item style={{ width: "12%" }}>
+            {/* to="/new-post" */}
+            <Link to="/" onClick={logout} color="inherit">
+              Log out
+            </Link>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
 export default NavBar;
---
+++ ex-pat-journal/src/components/theme.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+  btnColor: "#f0fff0", //Honeydew
+  backColor: "#faf0e6", //linen
+  secondBtnColor: "#faebd7", //antiqueWhite
+  textColor: "#2f4f4f", // dark slate gray
+  errorColor: "#b22222", // Firebrick red
+  padding: {
+    small: "2px",
+    medium: "10px",
+    large: "16px",
+  },
+  titleColor: "#191970", //midnight blue
+  width: {
+    small: "20%",
+    medium: "40%",
+    large: "60%",
+  },
+};
