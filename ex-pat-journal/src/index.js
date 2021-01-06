e75a52cb3515b63ac11727e99ca6ff0aeaa1a812
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-01-06 12:48:01 -0500

    working on sticky nav

--- ex-pat-journal/src/index.js
+++ ex-pat-journal/src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
-import logger from 'redux-logger';
-
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+import logger from "redux-logger";
+import { ThemeProvider } from "styled-components";
+import theme from "./components/theme";
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 const rootElement = document.getElementById("root");
 
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>,
   rootElement
 );
